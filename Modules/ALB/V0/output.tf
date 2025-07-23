d2a5ddd6044416c5d592d2f187101ed30b47ea79
Mayur Chaudhari <mayurchaudhari1172@gmail.com>
2025-07-23 13:46:34 +0530

    update all file

--- Modules/ALB/V0/output.tf
+++ Modules/ALB/V0/output.tf
@@ -6,15 +6,10 @@ output "lb_dns_name" {
   value = aws_lb.this.dns_name
 }
 
-<<<<<<< HEAD
 output "frontend_target_group_arn" {
   value = aws_lb_target_group.frontend_tg.arn
 }
 
 output "backend_target_group_arn" {
   value = aws_lb_target_group.backend_tg.arn
-=======
-output "alb_target_group_arn" {
-  value = aws_lb_target_group.this.arn
->>>>>>> be0342becaaa3a6779377983fef992cc55f335af
 }
